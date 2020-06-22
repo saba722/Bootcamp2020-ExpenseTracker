634512b71068d1ea121e1be4e498a7cf767ff649
Saba <sabaazam306@gmail.com>
2020-06-22 22:05:13 +0500

    Updated AccountSummary.js

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -5,12 +5,12 @@ export const AccountSummary = () => {
     const { transactions } = useContext(GlobalContext);
     const transactionAmounts = transactions.map(transaction => transaction.transactionAmount);
 
-    const income = transactionAmount
+    const income = transactionAmounts
         .filter(transaction => transaction > 0)
         .reduce((acc, transaction) => (acc += transaction), 0)
         .toFixed(2);
 
-    const expense = Math.abs(transactionAmount
+    const expense = Math.abs(transactionAmounts
         .filter(transaction => transaction < 0)
         .reduce((acc, transaction) => (acc += transaction), 0)
         .toFixed(2));

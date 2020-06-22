e1ce4ffd15f13cd76a0636178f004a3ffe03d3e6
Saba <sabaazam306@gmail.com>
2020-06-22 22:02:10 +0500

    Updated Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,7 +4,7 @@ import { GlobalContext } from '../context/GlobalState'
 export const Balance = () => {
 
     const { transactions } = useContext(GlobalContext);
-    const transactionAmounts = transactions.map(transaction.transactionAmount);
+    const transactionAmounts = transactions.map(transaction => transaction.transactionAmount);
     const balance = transactionAmounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
     return (

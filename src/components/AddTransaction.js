9daaf30888fb1d788f2fa0b5d87d6d55206c5962
Saba <sabaazam306@gmail.com>
2020-06-22 20:39:46 +0500

    Created Hook for Updating State with AddTransaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddTransaction = () => {
+    
+    const [description, setDescription] = useState('');
+    const [transactionAmount, setTransactionAmount] = useState(0);
+
     return (
         <div>
             <h3>Add New Transaction</h3>
@@ -9,13 +13,25 @@ export const AddTransaction = () => {
                     <label htmlFor="description">
                         Description
                     </label>
-                    <input type="text" placeholder="Detail of Transaction" />
+                    <input 
+                            type="text" 
+                            placeholder="Detail of Transaction"
+                            value={description} 
+                            id="description"
+                            onChange={(e) => setDescription(e.target.value)}
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="transactionamount">
                         Transaction Amount
                     </label>
-                    <input type="number" placeholder="Dollar Value of Transaction" />
+                    <input 
+                        type="number" 
+                        placeholder="Dollar Value of Transaction"
+                        id="transactionamount"
+                        value={transactionAmount}
+                        onChange={(e) => setTransactionAmount(e.target.value)}
+                    />
                 </div>
                 <button className="btn">Add Transaction</button>
             </form>

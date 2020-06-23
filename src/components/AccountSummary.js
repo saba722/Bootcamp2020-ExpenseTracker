87e92c539c099687e90354d0eb6e1244edcee840
Saba <sabaazam306@gmail.com>
2020-06-23 17:17:51 +0500

    Updated CSS

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -17,13 +17,13 @@ export const AccountSummary = () => {
     return(
         <div className="inc-exp-container">
             <div>
-                <h4>Income</h4>
+                <h4 className="clr-blue">Income</h4>
                 <p className="money plus">
                     {income}
                 </p>
             </div>
             <div>
-                <h4>Expense</h4>
+                <h4 className="clr-blue">Expense</h4>
                 <p className="money minus">
                     {expense}
                 </p>
